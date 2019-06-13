b292d3d27988458704e5b34c3727f0b9088149b1
James Carmichael <james@siteclick.co.uk>
2019-06-13 17:43:19 +0100

    Fix issues with scheduled renewal email and error handling

--- src/controllers/tasks/scheduleRenewalEmails.js
+++ src/controllers/tasks/scheduleRenewalEmails.js
@@ -1,4 +1,6 @@
-// const Notify = require('../notify');
+const { throwIfError } = require('@envage/hapi-pg-rest-api');
+const { get, find } = require('lodash');
+
 const messageQueue = require('../../lib/message-queue');
 const { enqueue } = require('../../modules/notify')(messageQueue);
 
@@ -6,63 +8,72 @@ const crm = {
   documents: require('../../lib/connectors/crm/documents')
 };
 const Permit = require('../../lib/connectors/permit');
-// const moment = require('moment');
 
-async function run (data) {
-  // send request to CRM to select licences with expiry in the next N days
+const { logger } = require('../../logger');
 
-  // http://127.0.0.1:8004/crm/1.0/expiring_licences?filter={"licence_type_id":8,"licence_regime_id":1}
-
-  const expiring = await Permit.expiringLicences.findMany();
-
-  // 18/54/08/0538
-  expiring.data.forEach(async (licence) => {
-    const responsedata = await crm.documents.getDocumentRoles({
-      role: 'primary_user',
-      system_external_id: licence.licence_ref
-    });
-
-    if (responsedata.data[0] && responsedata.data[0].individual_nm) {
-      const recipient = responsedata.data[0].individual_nm;
-
-      try {
-        // Read CRM doc header to get document custom name
-        const { data, error } = await crm.documents.getDocument(responsedata.data[0].document_id);
-
-        if (error) {
-          throw error;
-        }
-
-        const config = {
-          id: `${licence.licence_ref}_${licence.licence_end_dt}_${recipient}`,
-          recipient,
-          messageRef: 'expiry_notification_email',
-          personalisation: {
-            licence_no: licence.licence_ref,
-            licence_name: data.document_name || ''
-          },
-          individualEntityId: responsedata.data[0].individual_entity_id,
-          companyEntityId: responsedata.data[0].company_entity_id,
-          licences: [licence.licence_ref]
-        };
-
-        await enqueue(config);
-      } catch (error) {
-        return { error };
-      }
-    }
+const getDocument = async permitId => {
+  // Read CRM doc header to get document custom name
+  const [ document ] = await crm.documents.findAll({
+    system_internal_id: permitId
   });
+  return document;
+};
 
-  // get contact details for those licences
-
-  // check if notification has been sent
-
-  // schedule notifications
+const createMessageConfig = (licence, role, document) => {
+  const { entityName: recipient } = role;
   return {
-    error: null
+    id: `${licence.licence_ref}_${licence.licence_end_dt}_${recipient}`,
+    recipient,
+    messageRef: 'expiry_notification_email',
+    personalisation: {
+      licence_no: licence.licence_ref,
+      licence_name: document.document_name || ''
+    },
+    individualEntityId: role.entityId,
+    companyEntityId: document.company_entity_id,
+    licences: [licence.licence_ref]
   };
-}
+};
+
+const scheduleForLicence = async licence => {
+  // Get CRM doc
+  const document = await getDocument(licence.licence_id);
+
+  // Get document users from CRM to find document user with primary_user role
+  const { document_id: documentId } = document;
+  const { data, error } = await crm.documents.getDocumentUsers(documentId);
+  throwIfError(error);
+
+  const role = find(data, entity => entity.roles.includes('primary_user'));
+
+  // If role found, enqueue message
+  if (role) {
+    const config = createMessageConfig(licence, role, document);
+    await enqueue(config);
+  }
+};
+
+const scheduleRenewalEmails = async () => {
+  try {
+    // Get expiring permits
+    const { error, data: licences } = await Permit.expiringLicences.findMany();
+    throwIfError(error);
+
+    const tasks = licences.map(scheduleForLicence);
+
+    await Promise.all(tasks);
+
+    return { error: null };
+  } catch (err) {
+    const data = {
+      statusCode: get(err, 'statusCode'),
+      uri: get(err, 'options.uri')
+    };
+    logger.error('Error sending scheduled reminder letters', data);
+    return { error: err.toString() };
+  }
+};
 
 module.exports = {
-  run
+  run: scheduleRenewalEmails
 };
